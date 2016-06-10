0203566a402cdee02d6026ccfc26256fbbea651d
knut <knut.stenmark@gmail.com>
2016-06-10 08:47:29 +0200

    Ensure we do not loose overridden table_name in development environment upon reload.

--- lib/domain_neutral/engine.rb
+++ lib/domain_neutral/engine.rb
@@ -1,5 +1,6 @@
 module DomainNeutral
   class Engine < ::Rails::Engine
     isolate_namespace DomainNeutral
+    paths['app/models'].autoload_once! # Ensure we do not loose table_name definition in development environment if overridden
   end
 end
